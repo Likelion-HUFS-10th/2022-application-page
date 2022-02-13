73ead7d116edea1fefaa4bc4f70473760eec65db
seunghee jin <sualng123@gmail.com>
2022-02-14 01:37:26 +0900

    feat:create models with extra info

--- likelion10th/apply/admin.py
+++ likelion10th/apply/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Apply
 
+#account의 admin을 참고해주세요
 @admin.register(Apply)
 class Applyadmin(admin.ModelAdmin):
     """"Admin View for Apply"""
@@ -9,6 +10,11 @@ class Applyadmin(admin.ModelAdmin):
         "user",
         "is_completed",
     )
+    #admin에서 수정할 수 없는 그러니까 오직 읽을 수만 있는 필드를 지정합니다. 
     readonly_fields = (
         "user",
+        "first_q",
+        "second_q",
+        "third_q",
+        "fourth_q",
     )
