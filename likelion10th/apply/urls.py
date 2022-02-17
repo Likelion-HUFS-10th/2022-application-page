cb88ecf829dd3f2d1f9828bd6b5e67a7c8d31d03
Sohyun Eom <sohy19@hufs.ac.kr>
2022-02-17 12:30:41 +0900

    feat : apply 폼 create, update

--- likelion10th/apply/urls.py
+++ likelion10th/apply/urls.py
@@ -4,4 +4,6 @@ from . import views #현재 폴더에 있는 views에 접근하기 때문
 app_name = 'apply' # app_name에는 앱 이름을 넣어줍니다.
 
 urlpatterns = [
+    path('create/', views.create, name='create'),
+    path('update/<int:id>', views.update, name='update'),
 ]
--- likelion10th/apply/views.py
+++ likelion10th/apply/views.py
@@ -1,5 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Apply
+from account.models import User
+from django.contrib.auth.forms import AuthenticationForm
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def create(request):
+    if request.method == 'POST':
+        new_application = Apply()
+        form = AuthenticationForm(request=request, data=request.POST)
+        username = form.cleaned_data.get('username')
+        user = User().objects.filter(username=username)
+        new_application.user = user.name    # user
+        new_application.category = request.POST['category']   # category
+        new_application.study_url = request.POST['study_url']   # 깃헙 / 블로그 주소
+        new_application.first_q = request.POST['q1']   # 질문 1
+        new_application.second_q = request.POST['q2']   # 질문 2
+        new_application.third_q = request.POST['q3']   # 질문 3
+        new_application.fourth_q = request.POST['q4']   # 질문 4
+        new_application.save()
+        return render(request, 'detail.html')   # detail 페이지로
+    return render(request, 'create.html')
+
+def update(request, id):
+    form = Apply.objects.get(id=id)
+    if request.method == 'POST':
+        # form.user = ?
+        form.category = request.POST['category']   # category
+        form.study_url = request.POST['study_url']   # 깃헙 / 블로그 주소
+        form.first_q = request.POST['q1']   # 질문 1
+        form.second_q = request.POST['q2']   # 질문 2
+        form.third_q = request.POST['q3']   # 질문 3
+        form.fourth_q = request.POST['q4']   # 질문 4
+        form.save()
+        return redirect('apply:detail', form.id)
+    return render(request, 'update.html', {'apply': form})
