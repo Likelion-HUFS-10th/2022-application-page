f1635003d72e47f717fed53d1ef4d46582ae4ec3
Young jun Han <youngjunhan@Youngui-MacBookAir.local>
2022-02-19 10:49:26 +0900

    fixed:pass_or_fail

--- likelion10th/pass_or_fail/views.py
+++ likelion10th/pass_or_fail/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, get_object_or_404, redirect
 import account.models
-# Create your views here.
+from django.contrib.auth.decorators import login_required
+
+@login_required
+
 def pass_or_fail(request):
 
     User = request.user #로그인된 유저 뽑아내기
@@ -13,6 +16,3 @@ def pass_or_fail(request):
     else : 
         return render(request, 'pass.html')
        
-
-def anne(request):
-    return render(request, 'anne.html')
